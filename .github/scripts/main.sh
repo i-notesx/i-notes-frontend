837579ca45a78a98a6f93f3378af5d75f5707b4b
lwj <jg15575446613@163.com>
2025-01-05 17:02:58 +0800

    chore: ci/cd

--- .github/scripts/main.sh
+++ .github/scripts/main.sh
@@ -2,9 +2,14 @@
 
 # 项目路径
 project_dir="/var/www/i-notes"
+# 项目名称
+project_name="i-notes-frontend"
 # 服务名
 service_name="backend"
 
-cd "$project_dir"
+cd "$project_dir/$project_name"
+git pull
 
-docker-compose restart "$service_name"
+cd "$project_dir"
+docker-compose build "$service_name"
+docker-compose up -d "$service_name"
